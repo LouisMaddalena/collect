e35c6d382ffe3125279457cdece39bb79ea8adcc
LouisMaddalena <62859982+LouisMaddalena@users.noreply.github.com>
2023-03-21 20:48:41 -0400

    Update new_collect_file.py
    In the previous verion of the script I had used "content.txt" instead of "collect.txt"  Which is not compatible with the main script. 
    
    In this version, I have corrected that error, and I have also created a method by which you can change the name of the txt file using a --fix command.

--- new_collect_file.py
+++ new_collect_file.py
@@ -1,24 +1,54 @@
 import os
 import datetime
+import argparse
 
 #Simple accompanying script to create new collect.txt files in all folders and sub folders within a given path
 #Within each content.txt file the original creation date of that folder will be written in sortable date format
 
-def create_content_file(folder_path):
-    content_file = os.path.join(folder_path, 'content.txt')
-    if not os.path.exists(content_file):
+#Script also has option to rename files run as follows:
+# Python3 new_collect_file.py --fix "conllect.txt" "wrong_file.txt" 
+
+
+
+
+def create_collect_file(folder_path):
+    collect_file = os.path.join(folder_path, 'collect.txt')
+    if not os.path.exists(collect_file):
         folder_creation_time = os.path.getctime(folder_path)
         creation_date = datetime.datetime.fromtimestamp(folder_creation_time).strftime("Date:%Y_%m_%d")
-        with open(content_file, 'w') as file:
-            file.write(creation_date)
+        category = "Category:"
+        with open(collect_file, 'w') as file:
+            file.write(f"{creation_date}\n{category}")
 
-def crawl_directory_tree(start_path):
+def fix_mispelled_file(folder_path, correct_name, mispelled_name):
+    mispelled_file = os.path.join(folder_path, mispelled_name)
+    correct_file = os.path.join(folder_path, correct_name)
+    if os.path.exists(mispelled_file) and not os.path.exists(correct_file):
+        os.rename(mispelled_file, correct_file)
+
+def crawl_directory_tree(start_path, fix=False, correct_name=None, mispelled_name=None):
     for root, dirs, files in os.walk(start_path):
-        create_content_file(root)
+        if fix:
+            fix_mispelled_file(root, correct_name, mispelled_name)
+        else:
+            create_collect_file(root)
         for dir in dirs:
             subdir_path = os.path.join(root, dir)
-            create_content_file(subdir_path)
+            if fix:
+                fix_mispelled_file(subdir_path, correct_name, mispelled_name)
+            else:
+                create_collect_file(subdir_path)
 
 if __name__ == '__main__':
-    start_path = input("Parent Director to Add Content.txt files to: ") 
-    crawl_directory_tree(start_path)
+    parser = argparse.ArgumentParser(description='Create collect.txt files in directories.')
+    parser.add_argument('--fix', nargs=2, metavar=('correct_name', 'mispelled_name'), help='Fix mispelled filenames')
+
+    args = parser.parse_args()
+
+    start_path = input("Begin Directory Tree Path: ")  # Replace with your directory path
+
+    if args.fix:
+        correct_name, mispelled_name = args.fix
+        crawl_directory_tree(start_path, fix=True, correct_name=correct_name, mispelled_name=mispelled_name)
+    else:
+        crawl_directory_tree(start_path)
